47989090571310694fc5cdc8134d4ef7569c948f
Jorge Rojas <jorgearojas25@gmail.com>
2020-08-25 21:37:21 -0500

    First look at our components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-const Pet = ({ name, animal, bread }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, bread),
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Neko", animal: "Cat", bread: "black" }),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      bread: "white",
-    }),
-    React.createElement(Pet, {
-      name: "Alex",
-      animal: "Dog",
-      bread: "orange",
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
